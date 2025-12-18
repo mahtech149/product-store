5450eff509d8f1224dd8b07a2c389cac1d6442d9
mahtech149 <mahtech149@gmail.com>
2025-12-18 05:50:16 -0800

    corriger server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import path from "path";
 
 import { connectDB } from "./config/db.js";
-
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
@@ -11,16 +10,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// مهم مع ES Modules
 const __dirname = path.resolve();
 
-app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(express.json());
 
+// routes API
 app.use("/api/products", productRoutes);
 
+// serving frontend in production
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+	// ✅ fix هنا
+	app.get("/*", (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "frontend", "dist", "index.html")
+		);
 	});
 }
 
